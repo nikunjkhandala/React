1f57df64f9aa1d90f6834d4a193315da3f9af74f
nikunjkhandala <khandalanikunj286@gmail.com>
2026-01-26 18:57:16 +0530

    learn how to make react folder

---
+++ app.js
@@ -0,0 +1,8 @@
+export var a = 20;
+
+const user = {
+    name: "John",
+    age: 30
+}
+
+export default user;
---
+++ react_laren/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="parent">
+      <h1 id="child1">App </h1>
+      <h2 id="child2">hello</h2>
+      </div>
+      <div id="child3">
+        <h2>nikunj</h2>
+      </div>
+      </>
+  )
+}
+
+export default App
